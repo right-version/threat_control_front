6572a95cf0379ca31a0ae21ec082cb98ceb62958
Di <divasilevski@gmail.com>
2020-08-21 21:52:00 +0300

    to deploy

---
+++ dist/precache-manifest.3b8996cd82b98a416e1d40893ad3e7aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c746768b279bcb2c3f",
+    "url": "/css/app.eca8e225.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0247c298a9ec90800b52443deb05f386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847bbf651f6fe4aed604",
+    "url": "/js/about.d353daee.js"
+  },
+  {
+    "revision": "c9c746768b279bcb2c3f",
+    "url": "/js/app.32baa141.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "6876aba77d346e4f4fc84294d43dd9b2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
