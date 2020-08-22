c31d8beac60999a0420dbdbc858920c619736b76
Di <divasilevski@gmail.com>
2020-08-22 09:54:52 +0300

    Отображение страниц

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -19,66 +19,69 @@ export default {
     let counterBad = 0;
 
     if (this.response === undefined || this.response.data === undefined) {
-      this.$router.push("/");
-    } else {
-      const values = this.response.data;
-      values.forEach((value) => (value > 0.5 ? counterBad++ : counterGood++));
-
-      // const scatter = values.map((value) => ({ x: value[1], y: value[2] }));
-
-      const dataChartPie = {
-        type: "pie",
-        data: {
-          datasets: [
-            {
-              data: [counterGood, counterBad],
-              backgroundColor: ["#3e95cd", "#8e5ea2"],
-            },
-          ],
-          labels: ["Good", "Bad"],
-        },
-
-        options: {
-          title: {
-            display: true,
-            text: "Нейросеть графики",
-          },
-          responsive: true,
-          animation: {
-            duration: 1.5,
-            easing: "linear",
-          },
-        },
-      };
-
-      // const dataChartScatter = {
-      //   type: "scatter",
-      //   data: {
-      //     datasets: [
-      //       {
-      //         label: "Scatter Dataset",
-      //         data: scatter,
-      //         pointBorderColor: new Color("pink"),
-      //         pointBackgroundColor: new Color("#ff0000"),
-      //       },
-      //     ],
-      //   },
-      //   options: {
-      //     scales: {
-      //       xAxes: [
-      //         {
-      //           type: "linear",
-      //           position: "bottom",
-      //         },
-      //       ],
-      //     },
-      //   },
-      // };
-      // console.log(this.response.data)
-
-      this.createChart("chartPie", dataChartPie);
-      // this.createChart("chartScatter", dataChartScatter);
+      this.response.data = []
+      for (let i = 0; i < 1000; i++) {
+        this.response.data.push(Math.random())
+      }
+      // this.$router.push("/");
     }
+    const values = this.response.data;
+    values.forEach((value) => (value > 0.5 ? counterBad++ : counterGood++));
+
+    // const scatter = values.map((value) => ({ x: value[1], y: value[2] }));
+
+    const dataChartPie = {
+      type: "pie",
+      data: {
+        datasets: [
+          {
+            data: [counterGood, counterBad],
+            backgroundColor: ["#3e95cd", "#8e5ea2"],
+          },
+        ],
+        labels: ["Good", "Bad"],
+      },
+
+      options: {
+        title: {
+          display: true,
+          text: "Нейросеть графики",
+        },
+        responsive: true,
+        animation: {
+          duration: 1.5,
+          easing: "linear",
+        },
+      },
+    };
+
+    // const dataChartScatter = {
+    //   type: "scatter",
+    //   data: {
+    //     datasets: [
+    //       {
+    //         label: "Scatter Dataset",
+    //         data: scatter,
+    //         pointBorderColor: new Color("pink"),
+    //         pointBackgroundColor: new Color("#ff0000"),
+    //       },
+    //     ],
+    //   },
+    //   options: {
+    //     scales: {
+    //       xAxes: [
+    //         {
+    //           type: "linear",
+    //           position: "bottom",
+    //         },
+    //       ],
+    //     },
+    //   },
+    // };
+    // console.log(this.response.data)
+
+    this.createChart("chartPie", dataChartPie);
+    // this.createChart("chartScatter", dataChartScatter);
   },
   methods: {
     createChart(chartId, chartData) {
