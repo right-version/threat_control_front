76bf343d7617e649661de5a4a06b0365b484a7ba
Di <divasilevski@gmail.com>
2020-08-23 01:02:43 +0300

    Логика для истории

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
-  .history-page.container
-    .home__header-link
-      a(@click='$router.go(-1)').back Back
+.history-page.container
+  .home__header-link
+    a.back(@click="$router.go(-1)") Back
 
-    .history-page__data(v-if='dataset')
-    template(v-for='link in dataset' :keys="link.key")
-      router-link(:to="'/?result='+link.key") 
-        p {{ link }}
-        
+  .history-page__data(v-if="dataset")
+    template(v-for="link in dataset.slice(0, 10)", :keys="link.key")
+      .history-page__block.history-page__block_type_good(v-if='link.info' @click="goTo(link.key)")
+        .history-page__icon X
+        .history-page__status {{ getStatus(link.info.counterBad) }}
+        .history-page__data {{ link.info.date }}
 </template>
 
 <script>
@@ -19,20 +20,44 @@ export default {
     badlinks: true,
   }),
   mounted() {
-    api
-      .getKeys(this.$http)
-      .then(data => {
-        this.dataset = Object.keys(data).map(values => data[values].key)
-        console.log(this.dataset)
-      })
+    api.getKeys(this.$http).then((data) => {
+      this.dataset = Object.keys(data).map((values) => data[values].key).reverse();
+    });
+  },
+  methods: {
+    getStatus(value) {
+      if (value < 30) {
+        return 'Угроз не обнаружено'
+      } else if (value < 60) {
+        return 'Незначительное кол-во угроз'
+      } else {
+        return 'Угрозы обнаружены'
+      }
+    },
+    goTo(value) {
+      this.$router.push('/?result=' + value)
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-  .history-page {
-    .home__header-link {
-      margin-top: 30px;
-    }
+.history-page {
+  .home__header-link {
+    margin-top: 30px;
   }
+
+  .history-page__block {
+    .history-page__icon {}
+    .history-page__status {}
+    .history-page__data {}
+  }
+
+  .history-page__block_type_good {}
+  .history-page__block_type_norm {}
+  .history-page__block_type_bad {}
+
+}
+
+        
 </style>
