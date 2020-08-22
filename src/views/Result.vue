d148e6b93c88182f6e1b10dff11806897c2d3d21
Di <divasilevski@gmail.com>
2020-08-22 09:03:01 +0300

    Графики

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,17 +1,105 @@
 <template lang="pug">
-  .home
-    router-link(to='/') Home <->
-    .h1 Конкретный рассчет
-    p {{ this.response }}
+.home
+  router-link(to="/") Home <->
+  .h1 Конкретный рассчет
+  canvas#chartPie
+  canvas#chartScatter
 </template>
 
 
 <script>
-import { mapState } from 'vuex'
+import Chart from "chart.js";
+import { mapState } from "vuex";
 
 export default {
   computed: {
-    ...mapState(['response']),
-  }
-}
+    ...mapState(["response"]),
+  },
+  data: () => ({
+    statusMessage: "Загрузка",
+  }),
+  mounted() {
+    let counterGood = 0;
+    let counterBad = 0;
+
+    if (this.response === undefined || this.response.data === undefined) {
+      this.statusMessage = "Данных нет";
+    }
+    console.log(this.response.data)
+    const values = Object.values(this.response.data);
+    values.forEach((value) => (value[0] > 0.3 ? counterBad++ : counterGood++));
+
+    const scatter = values.map((value) => ({ x: value[1], y: value[2] }));
+
+    const dataChartPie = {
+      type: "pie",
+      data: {
+        datasets: [
+          {
+            data: [counterGood, counterBad],
+            backgroundColor: ["#3e95cd", "#8e5ea2"],
+          },
+        ],
+        labels: ["Good", "Bad"],
+      },
+
+      options: {
+        title: {
+          display: true,
+          text: "Нейросеть графики",
+        },
+        responsive: true,
+        animation: {
+          duration: 1.5,
+          easing: "linear",
+        },
+      },
+    };
+
+    const dataChartScatter = {
+      type: "scatter",
+      data: {
+        datasets: [
+          {
+            label: "Scatter Dataset",
+            data: scatter,
+            pointBorderColor: new Color("pink"),
+            pointBackgroundColor: new Color("#ff0000"),
+          },
+        ],
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: "linear",
+              position: "bottom",
+            },
+          ],
+        },
+      },
+    };
+    console.log(this.response.data)
+    this.statusMessage = "";
+    this.createChart("chartPie", dataChartPie);
+    this.createChart("chartScatter", dataChartScatter);
+  },
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById(chartId);
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options,
+      });
+    },
+  },
+};
 </script>
+
+<style lang="scss">
+.home {
+  max-width: 800px;
+  margin: auto;
+}
+</style>
